86b0d6a35a9e0f091336dd56c6601117a5e2c237
KubraIsik <kuisik@gmail.com>
2024-09-13 00:44:43 +0300

    first design

---
+++ script.js
@@ -0,0 +1,11 @@
+// Function to show the popup
+function showPopup(stepId) {
+    var popup = document.getElementById(stepId);
+    popup.style.display = "flex";
+}
+
+// Function to close the popup
+function closePopup(stepId) {
+    var popup = document.getElementById(stepId);
+    popup.style.display = "none";
+}
